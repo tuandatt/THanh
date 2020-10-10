4de7ed9246d7d99f3c779281a7e2243d6ad2943c
tuandatt <dangtuandat2106@gmail.com>
2020-10-10 21:41:03 +0700

    add

---
+++ TH1_bai1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+namespace bths1
+{
+    class DIEM
+    {
+        private double x, y;
+        public DIEM()
+        {
+            x = y = 0;
+        }
+        public DIEM(double x, double y)
+        {
+            this.x = x; this.y = y;
+        }
+        public void nhap()
+        {
+            Console.Write("nhập x: "); x = double.Parse(Console.ReadLine());
+            Console.Write("nhập y: "); y = double.Parse(Console.ReadLine());
+        }
+        public void hien()
+        {
+            Console.WriteLine("tọa độ là: {0},{1} ", x, y);
+        }
+        public double khoangcach(DIEM b)
+        {
+            return Math.Sqrt(Math.Pow(b.x - this.x, 2) + Math.Pow(b.y - this.y, 2));
+        }
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            DIEM a = new DIEM(); a.nhap(); a.hien();
+            DIEM b = new DIEM(); b.nhap(); b.hien();
+            Console.WriteLine("khoảng cách là: " + a.khoangcach(b));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TH1_bai2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace bai2
+{
+    class Employe
+    {
+        private int id, year;
+        private string name;
+        private double salary, basic;
+        public double setsalary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+        public double setbasic
+        {
+            get
+            {
+                return basic;
+            }
+            set
+            {
+                basic = value;
+            }
+        }
+        public int getid()
+        {
+            return id;
+        }
+        public string getname()
+        {
+            return name;
+        }
+        public int getyear()
+        {
+            return year;
+        }
+        public double getincome()
+        {
+            return salary * basic;
+        }
+        public void display()
+        {
+            Console.WriteLine("dinh danh\t ten\tnam sinh \tluong co ban\t thu nhap");
+            Console.WriteLine(id + "\t" + name + "\t" + year + "\t\t" + basic + "\t" + getincome());
+        }
+        static void Main(string[] args)
+        {
+            Employe e = new Employe();
+            e.display();
+            Console.ReadKey();
+        }
+    }
+}
