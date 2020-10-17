6cc914533369a1310e940fdabbd61a4f3777fc0a
tuandatt <dangtuandat2106@gmail.com>
2020-10-17 19:27:17 +0700

    add

---
+++ TH2_bai1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+
+namespace TH2_bai1
+{
+    class Stack
+    {
+        private int top;
+        private int[] a;
+        public bool empty()
+        {
+            return (top == -1);
+        }
+        public bool full()
+        {
+            return (top >= a.Length);
+        }
+        public Stack()
+        {
+            a = new int[20];
+            top = -1;
+        }
+        public void push(int x)
+        {
+            if (!full())
+            {
+                top = top + 1;
+                a[top] = x;
+            }
+            else
+                Console.Write("stack tran");
+        }
+        public int pop()
+        {
+            if (empty())
+            {
+                Console.Write("stack can");
+                return 0;
+            }
+            else
+                return a[top--];
+        }
+    }
+    class hecoso
+    {
+        static void Main1()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            int n;
+            Console.Write("Nhập số cần đổi:");
+            n = int.Parse(Console.ReadLine());
+            Stack T = new Stack();
+            string st = "0123456789ABCDEF";
+            while (n != 0)
+            {
+                T.push((int)st[n % 16]);
+                n = n / 16;
+            }
+            Console.Write("Kết quả sau khi đổi sang hệ 16 :");
+            while (!T.empty())
+            {
+                Console.Write("{0}", (char)T.pop());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TH2_bai2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+
+namespace TH2_bai2
+{
+    class PS
+    {
+        private int tu, mau;
+        public PS()
+        {
+            tu = 0;
+            mau = 0;
+        }
+        public PS(int x, int y)
+        {
+            this.tu = tu;
+            this.mau = mau;
+        }
+        public void nhap()
+        {
+            Console.Write("Nhập tử số: "); 
+            tu = int.Parse(Console.ReadLine());
+            Console.Write("Nhập mẫu số: "); 
+            mau = int.Parse(Console.ReadLine());
+        }
+        public void hien()
+        {
+            Console.Write("Phân số là: ");
+            Console.WriteLine($"{tu}/{mau}");
+        }
+        public int Uscln(int x, int y)
+        {
+            x = Math.Abs(x); y = Math.Abs(y);
+            while (x != y)
+            {
+                if (x > y) x = x - y;
+                if (y > x) y = y - x;
+            }
+            return x;
+        }
+        public PS rutgon()
+        {
+            int uc = Uscln(this.tu, this.mau);
+            this.tu = this.tu / uc;
+            this.mau = this.mau / uc;
+            return this;
+        }
+        public static PS operator +(PS t1, PS t2)
+        {
+            PS t = new PS();
+            t.tu = t1.tu * t2.mau + t1.mau * t2.tu;
+            t.mau = t1.mau * t2.mau;
+            return t;
+        }
+        public static PS operator -(PS t1, PS t2)
+        {
+            PS t = new PS();
+            t.tu = t1.tu * t2.mau - t1.mau * t2.tu;
+            t.mau = t1.mau * t2.mau;
+            return t;
+        }
+        public static PS operator /(PS t1, PS t2)
+        {
+            PS t = new PS();
+            t.tu = t1.tu * t2.mau;
+            t.mau = t1.mau * t2.tu;
+            return t;
+        }
+        public static PS operator *(PS t1, PS t2)
+        {
+            PS t = new PS();
+            t.tu = t1.tu * t2.tu;
+            t.mau = t1.mau * t2.mau;
+            return t;
+        }
+        public static bool operator >(PS t1, PS t2)
+        {
+            return t1.tu * t2.mau > t2.tu * t1.mau;
+        }
+        public static bool operator <(PS t1, PS t2)
+        {
+            return t1.tu * t2.mau < t2.tu * t1.mau;
+        }
+    }
+    class test
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            PS t = new PS();
+            t.nhap(); t.hien();
+            PS t2 = new PS();
+            t2.nhap(); t2.hien();
+            PS t3 = t + t2; t3.hien();
+            PS t4 = t * t2; t4.hien();
+            if (t > t2)
+            {
+                Console.WriteLine("Phân số t1 > t2");
+            }
+            else if (t < t2)
+            {
+                Console.WriteLine("Phân số t1 < t2");
+            }
+            else
+                Console.WriteLine("Hai phân số = nhau");
+            Console.ReadKey();
+        }
+    }
+}
